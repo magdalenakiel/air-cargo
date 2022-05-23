0d72e14e62cd863a8bac5ab1042fe5235303ecfa
Magda <magdalena.kiel00@gmail.com>
2022-05-23 18:28:35 +0200

    add upload zone

--- air-cargo.js
+++ air-cargo.js
@@ -1,3 +1,10 @@
+const dropZone = document.querySelector(".dropZone");
+let validExtensions = ["pdf", "jpeg", "png", "docx", "doc"]
+const nameFiles = document.getElementById('nameFiles');
+const inputHidden = document.getElementById('filesAddHidden');
+const buttonShow = document.getElementById('filesAddShow');
+var files = new Set();
+
 function addPayload() {
     const payloadRowHTML = `                       
         <td><input name="nameCargo" type="text" placeholder="Nazwa ładunku" required></td>
@@ -22,7 +29,60 @@ function removePayload(){
         payloadTable.deleteRow(rowCount - 1);
     }
 }
-
+buttonShow.onclick = ()=>{
+    inputHidden.click();
+}
+inputHidden.addEventListener("change", function(){
+    let documents = this.files;
+    addFiles(documents)
+});
+function pullFileExtensionFromName(f){
+    const spliteName = f.name.split('.');
+    const position = spliteName.length - 1;
+    return spliteName[position];
+}
+dropZone.addEventListener("dragover", (event)=>{
+    event.preventDefault();
+    dropZone.classList.add("active");
+});
+dropZone.addEventListener("dragleave", ()=>{
+    dropZone.classList.remove("active");
+});
+dropZone.addEventListener("drop", (event)=>{
+    event.preventDefault();
+    let documents = event.dataTransfer.files;
+    addFiles(documents)
+});
+function addFiles(files){
+    var totalfiles = files.length
+    console.log(totalfiles)
+    if(totalfiles > 0 ){
+        for(let i = 0; totalfiles > i; i++){
+            console.log(files[i].name)
+            addFilename(files[i])
+        }
+        printINDropZone();
+    }
+}
+function addFilename(separateFile){
+    let fileType = pullFileExtensionFromName(separateFile)
+    if(validExtensions.includes(fileType)){
+        files.add(separateFile)
+        // nameText += `${separateFile.name}<br>`
+        // console.log(nameText)
+    }else{
+        alert("Plik nie posiada odpowiedzniego rozszerzenia. Akceptowalne rozszerzenia: jpg, png, doc, docx, pdf")
+        dropZone.classList.remove("active");
+    }
+}
+function printINDropZone(){
+    let filenamesStr = "";
+    files.forEach(file => {
+        filenamesStr += `${file.name}<br>`
+        console.log(file)
+    });
+    nameFiles.innerHTML = filenamesStr;
+}
 addPayload();
 document.getElementById('removePayload').addEventListener('click', removePayload);
 document.getElementById('addPayload').addEventListener('click', addPayload);
